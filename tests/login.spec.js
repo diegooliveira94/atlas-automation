bf2b343fc7597f84c275d24fa5c07186802035a6
Diego Oliveira <diegooliveira-94@hotmail.com>
2025-09-03 11:06:15 +0100

    change button
    O force: true não funcionou, dupliquei o click para forçar o formulário a submeter

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -31,6 +31,7 @@ test.describe('Testes em sequência', () => {
     await page.locator('#GEN_INPUT_ACCOUNT_CREATE_FIRST_NAME').fill(nameUser);
     await page.locator('#GEN_INPUT_ACCOUNT_CREATE_PASSWORD').fill('teste@teste123');
     await page.getByRole('button', { name: 'Create account' }).click({force: true});
+    await page.getByRole('button', { name: 'Create account' }).click({force: true});
 
     // Esperar por uma resposta ou navegação
     try {
